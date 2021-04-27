a11a207884c6f4607a9fe9372604d50bce2817e8
rajeshsawant98 <79257254+rajeshsawant98@users.noreply.github.com>
2021-04-27 11:39:02 +0530

    update sizeof.cpp

--- sizeof.cpp
+++ sizeof.cpp
@@ -1,3 +1,7 @@
+/*Write a C++ Program to Find Size of int, float, double and char in Your System
+Your output should look something like this
+Size of char: 1 byte Size of int: 4 bytes Size of float: 4 bytes Size of double: 8 bytes */
+
 #include<iostream>
 using namespace std;
 
