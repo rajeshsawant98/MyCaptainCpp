0847d4845aae8f37f80cfc4276c1e9d7daa1a9cc
rajeshsawant98 <79257254+rajeshsawant98@users.noreply.github.com>
2021-04-27 11:36:46 +0530

    Created sizeof.cpp

---
+++ sizeof.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    float b;
+    char c;
+    double d;
+
+    cout<<"Size of int: "<<sizeof(a)<<" bytes"<<endl;
+    cout<<"Size of float: "<<sizeof(b)<<" bytes"<<endl;
+    cout<<"Size of char: "<<sizeof(c)<<" bytes"<<endl;
+    cout<<"Size of double: "<<sizeof(d)<<" bytes"<<endl;
+}
